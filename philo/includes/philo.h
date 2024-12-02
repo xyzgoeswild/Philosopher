13076ae5ab81334911d1250e13f877fa80e42120
xyz <122036097+xyzgoeswild@users.noreply.github.com>
2024-12-02 23:09:43 +0100

    Add files via upload

---
+++ philo/includes/philo.h
@@ -0,0 +1,16 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/* assist.c */
+int	white_space(char str);
+int	ft_atoi(const char *str);
+
+
+/* parsing.c */
+
+
+#endif
---
+++ philo/src/utils.c
@@ -0,0 +1,32 @@
+#include "philo.h"
+
+int	white_space(char str)
+{
+	return (str == ' ' || (str >= '\t' && str <= '\r'));
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	res;
+	int	sign;
+
+	i = 0;
+	res = 0;
+	sign = 1;
+	while (white_space(str[i]))
+		i++;
+	if ((str[i] == '-' || str[i] == '+')
+		&& (str[i + 1] >= '0' && str[i + 1] <= '9'))
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - 48);
+		i++;
+	}
+	return (res * sign);
+}
